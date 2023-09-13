cdd9c2beba0feb3937d899eddfaff771b944d54a
MartaVelilla <202002581@alu.comillas.edu>
2023-09-13 13:06:08 -0400

    login

--- README.md
+++
@@ -1,2 +0,0 @@
-# EC463_MiniProject
-Web Chat Software Design Mini Project for Senior Design
---
+++ src/components/chat.js
@@ -0,0 +1,14 @@
+// components/Chat.js
+import React from 'react';
+
+function Chat() {
+  // Aquí colocarás la lógica de tu aplicación de chat
+  return (
+    <div>
+      {/* Contenido del chat */}
+      <h2>HOLA soy chat</h2>
+    </div>
+  );
+}
+
+export default Chat;
