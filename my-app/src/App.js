b30a1d71e468963835056926002c3ad851bc5a36
NoahCherry13 <noahd1326@gmail.com>
2023-09-14 17:11:33 -0400

    functional chat app
    ugly but functional chat app with firebase backend

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import './App.css';
 
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
+import 'firebase/compat/analytics'
 
 import {useAuthState} from 'react-firebase-hooks/auth';
 import {useCollectionData} from 'react-firebase-hooks/firestore';
@@ -20,6 +21,21 @@ firebase.initializeApp({
 
 const auth = firebase.auth();
 const firestore = firebase.firestore();
+const analytics = firebase.analytics();
+
+function App() {
+  const [user] = useAuthState(auth);
+
+  return (
+    <div className="App">
+      <section>
+       {user ? <ChatRoom /> :<SignIn />}
+      </section>
+      <SignOut />
+
+    </div>
+  );
+}
 
 function SignIn(){
   const signInWithGoogle = () =>{
@@ -31,22 +47,6 @@ function SignIn(){
   )
 }
 
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-
-      </header>
-      <section>
-        <SignIn />
-      {/* {user ? <ChatRoom /> :<SignIn />} */}
-      </section>
-    </div>
-  );
-}
-
-
 function SignOut(){
   return auth.currentUser && (
     <button onClick={()=> auth.signOut()}>Sign Out</button>
@@ -54,23 +54,52 @@ function SignOut(){
 }
 
 function ChatRoom(){
+  const dummy = useRef();
   const messagesRef = firestore.collection('messages');
   const query = messagesRef.orderBy('createdAt').limit(25);
-  const [messages] = useCollectionData(query, {idField: 'id'});
+  const [messages] = useCollectionData(query, { idField: 'id' });
+  const [formValue, setFormValue] = useState('');
+
+  const sendMessage = async(e) => {
+    e.preventDefault();
+    const {uid, photoURL} = auth.currentUser;
+
+    await messagesRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL
+    })
+
+    setFormValue('');
+  }
+
 
   return (
     <>
-      <div>
-        {messages && messages.map( msg => <ChatMessage key ={msg.id} message = {msg} />)}
-      </div>
+      <main>
+        {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+      </main>
+
+      <form onSubmit={sendMessage}>
+        <input value={formValue} onChange={(e) => setFormValue(e.target.value)}/>
+        <button type="submit"></button>
+      </form>
     </>
   )
 }
 
-function ChatMessage(props){
-  const {text, uid} = props.message;
+function ChatMessage(props) {
+  const { text, uid, photoURL } = props.message;
 
-  return <p>{text}</p>
+  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+
+  return (<>
+    <div className={`message ${messageClass}`}>
+      <img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} />
+      <p>{text}</p>
+    </div>
+  </>)
 }
 
 export default App;
